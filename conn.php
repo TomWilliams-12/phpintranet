2ca13e2b702f8734a48a22ec928186e53b2c596a
Tom Williams <williams.t17@gmail.com>
2020-08-26 10:26:32 +0100

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,20 @@
+<?php
+
+$dbServername ="eu-cdbr-west-03.cleardb.net/heroku_c9893a73f923e06";
+$dbUsername = "b56cb063f2b2a6";
+$dbPassword = "e39651ee";
+$dbName = "heroku_c9893a73f923e06";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+
+// sql to create table
+// $sql = "CREATE TABLE meterReadings (
+//     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//     siteName VARCHAR(30) NOT NULL,
+//     department VARCHAR(30) NOT NULL,
+//     serialNumber VARCHAR(200),
+//     blackReading VARCHAR(200),
+//     colourReading VARCHAR(200)
+//     )";
